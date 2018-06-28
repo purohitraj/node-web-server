63fe3c37f93fab5d182f23b448cc4dda0e4574f7
raj <raj.purohit@workhiveapp.com>
2018-06-28 23:55:51 +0530

    'hii'

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 var app = express();
 
-
+const port = process.env.PORT || 3000; 
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine', 'hbs');
 
@@ -28,6 +28,6 @@ app.get('/about',(req,res)=>{
 
 });
 
-app.listen(3000,()=>{
-	console.log('server is on ');
+app.listen(port,()=>{
+	console.log(`server is on ${port}`);
 });
